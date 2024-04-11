2c85877bf4c327d7fa35f6a436826123f176f080
吳 蕥綺 <n0975116268@gmail.com>
2024-04-11 21:34:47 +0800

    Fix Api Url

--- src/apis/product.js
+++ src/apis/product.js
@@ -1,7 +1,7 @@
 import { request } from "../utils/request";
 export async function listProducts() {
     try {
-        const response = await request("http://localhost:8080/products" );
+        const response = await request("/api/products" );
         return response.results; 
     } catch (error) {
         console.error('Error fetching products:', error);
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      "/api": "http://localhost:8080",
-    },
-  },
-});
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+})
